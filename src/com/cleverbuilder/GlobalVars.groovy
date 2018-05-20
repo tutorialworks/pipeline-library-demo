69446781cad5f7b7864a6f9c784128673f33219d
Tom Donohue <monodot@gmail.com>
2018-05-20 22:08:22 +0100

    Further global var testing

--- src/com/cleverbuilder/GlobalVars.groovy
+++ src/com/cleverbuilder/GlobalVars.groovy
@@ -3,4 +3,9 @@ package com.cleverbuilder
 
 class GlobalVars {
    static String foo = "bar"
+
+   // refer to this in a pipeline using:
+   //
+   // import com.cleverbuilder.GlobalVars
+   // println GlobalVars.foo
 }
