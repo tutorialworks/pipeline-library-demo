380bb841717b8db910c1a7b44a9ce2c6d58d5053
Ramagopalan <Ramji_Ramagopalan@Dell.com>
2020-09-28 01:20:03 +0530

    checkout

--- vars/checkout.groovy
+++ vars/checkout.groovy
@@ -1,5 +1,9 @@
 #!/usr/bin/env groovy
 import jenkins.model.*
+import hudson.slaves.EnvironmentVariablesNodeProperty
+import hudson.EnvVars
+import org.jenkinsci.plugins.pipeline.modeldefinition.Utils
+import java.util.UUID
 jenkins = Jenkins.instance
 def call(String giturl = 'https://github.com/ramjigit/pipeline-library-demo.git') {
   echo "Hello, ${name}."
