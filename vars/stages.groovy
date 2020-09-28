6aed74e54c5bba5fdfc15985df093da9a42e49c0
Ramagopalan <Ramji_Ramagopalan@Dell.com>
2020-09-28 23:55:43 +0530

    adding stages

---
+++ vars/stages.groovy
@@ -0,0 +1,27 @@
+
+#!/usr/bin/env groovy
+
+def call() {
+stage('Source co') {
+    echo "Source code checkout"
+}
+stage('Compile') {
+    echo "Compile"
+}
+stage('Unit Test') {
+    echo "Unit test"
+}
+stage('Static Code Analysis') {
+    echo "Static Code Analysis"
+}
+stage('BVT') {
+    echo "BVT"
+}
+stage('Vulnerability Scanning') {
+    echo "Vulnerability Scanning"
+}
+stage('Artifactory Push') {
+    echo "Artifactory Push"
+}
+
+}
