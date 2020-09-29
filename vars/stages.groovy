03b1551e5a09e1489aa11b5a8c574ce24fc450c5
Ramagopalan <Ramji_Ramagopalan@Dell.com>
2020-09-29 14:00:50 +0530

    push to influx db

--- vars/stages.groovy
+++ vars/stages.groovy
@@ -20,6 +20,20 @@ stage('Vulnerability Scanning') {
 }
 stage('Artifactory Push') {
     echo "Artifactory Push"
+stage('Push to influxDB') {
+    echo "Pushing to infuxdb"
+    step([$class: 'InfluxDbPublisher',
+ customData: null,
+ customDataMap: null,
+ customDataMapTags: null,
+customDataTags: null,
+ customPrefix: '',
+ customProjectName: '',
+ jenkinsEnvParameterField: '',
+ jenkinsEnvParameterTag: '',
+measurementName: null,
+ selectedTarget: 'TestDB',
+ target: [$class: 'Target', database: 'rfdata', description: 'TestDB', exposeExceptions: true, globalListener: false, globalListenerFilter: '', jobScheduledTimeAsPointsTimestamp: true, password: <object of type hudson.util.Secret>, retentionPolicy: 'autogen', url: 'http://3.19.208.33:8086', username: 'admin', usingJenkinsProxy: true]])
 }
 
 }
