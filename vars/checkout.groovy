bab01521f203f1693b8ab47889b002a2fb6265dc
Ramagopalan <Ramji_Ramagopalan@Dell.com>
2020-09-30 16:34:12 +0530

    add

--- vars/checkout.groovy
+++ vars/checkout.groovy
@@ -1,6 +1,10 @@
 #!/usr/bin/env groovy
 
-def call()
-{
-checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'mygitid', url: 'https://github.com/ramjigit/pipeline-library-demo.git']]])
-}
+def call(Map stageParams) {
+ 
+    checkout([
+        $class: 'GitSCM',
+        branches: [[name:  stageParams.branch ]],
+        userRemoteConfigs: [[ url: stageParams.url ]]
+    ])
+  }
