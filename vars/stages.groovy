5792f3e1eea580154d351a233df01402462dd817
Ramagopalan <Ramji_Ramagopalan@Dell.com>
2020-09-30 12:56:56 +0530

    adding influxdb

--- vars/stages.groovy
+++ vars/stages.groovy
@@ -23,18 +23,7 @@ stage('Artifactory Push') {
 }    
 stage('Push to influxDB') {
     echo "Pushing to infuxdb"
-        step([$class: 'InfluxDbPublisher',
-         customData: null,
-         customDataMap: null,
-         customDataMapTags: null,
-         customDataTags: null,
-         customPrefix: '',
-         customProjectName: '',
-         jenkinsEnvParameterField: '',
-         jenkinsEnvParameterTag: '',
-         measurementName: null,
-         selectedTarget: 'TestDB',
-         target: [$class: 'Target', database: 'rfdata', description: 'TestDB', exposeExceptions: true, globalListener: false, globalListenerFilter: '', jobScheduledTimeAsPointsTimestamp: true, password: 'password12', retentionPolicy: 'autogen', url: 'http://3.19.208.33:8086', username: 'admin', usingJenkinsProxy: true]])
-  }
 
+   influxDbPublisher customPrefix: '', customProjectName: '', jenkinsEnvParameterField: '', jenkinsEnvParameterTag: '', selectedTarget: 'TestDB'
+  }
 }
