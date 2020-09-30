b6c15532274006b9e3e31a36a9f8bdb424c54f41
Ramagopalan <Ramji_Ramagopalan@Dell.com>
2020-09-30 17:58:53 +0530

    adding

--- vars/listnumbers1.groovy
+++ vars/listnumbers1.groovy
@@ -5,6 +5,14 @@ def call(val, x=10){
   for(i in 1..5){
     println i
   }
+  
 def arg = ["Groovy","Java","Python"]
+println arg[1]
 println arg.class
+def no = [1,2,3,4]
+def no2 = no +5 //=[1,2,3,4,5]
+def no3 = no - [2,3] //=[1,4]
+println "no is $no"
+println "no2 is $no2"
+println "no3 is $no3"
 }
