7d0a7d756da6ea424b8bc4e9b1d80f1d82dce94a
Ramagopalan <Ramji_Ramagopalan@Dell.com>
2020-10-01 22:43:45 +0530

    post to influxdb

--- vars/checkout.groovy
+++ vars/checkout.groovy
@@ -5,5 +5,9 @@ stage('Checkout') {
     echo "Source code checkout"
     def stagename='Checkout'
     echo "$stagename is completed"
+    post {
+            always {
+                influxDbPublisher(selectedTarget: 'TestDB', customData: assignURL(BUILD_URL))
+            }
 }
 }
