c012d63f9fcda5dd4d4e31dde980289c78f77cf2
Ramagopalan <Ramji_Ramagopalan@Dell.com>
2020-10-02 00:03:21 +0530

    post to influxdb

---
+++ vars/buidstage.groovy
@@ -0,0 +1,29 @@
+// vars/Build.groovy
+def call() {
+     pipeline {
+    
+    agent any
+
+    environment {
+        BUILD_URL = "${BUILD_URL}"
+    }
+
+    stages {
+        stage('Build') {
+            steps {
+                echo 'Building project...'
+            }
+        }
+        
+        post {
+            always {
+                influxDbPublisher(selectedTarget: 'TestDB', customData: assignURL(BUILD_URL))
+            }
+        }
+    }
+
+def assignURL(build_url) {
+    def buildURL = [:]
+    buildURL['url'] = build_url
+    return buildURL
+}}
