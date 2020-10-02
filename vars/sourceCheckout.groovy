ce5ab0a5a32a2b2a20f3e63668c7b8683dcc2107
Ramagopalan <Ramji_Ramagopalan@Dell.com>
2020-10-02 19:55:28 +0530

    add

---
+++ vars/sourceCheckout.groovy
@@ -0,0 +1,26 @@
+#!/usr/bin/env groovy
+def call() {
+ pipeline {
+    
+    agent any
+
+    environment {
+        BUILD_URL = "${BUILD_URL}"
+    }
+
+    stages {
+        stage('Build') {
+            steps {
+                echo 'Building project...'
+            }
+        }
+        
+        }
+        post {
+            always {
+                influxDbPublisher customPrefix: '', customProjectName: 'TestDB', jenkinsEnvParameterField: 'KEY=\'Build\'', jenkinsEnvParameterTag: 'BUILD_URL', selectedTarget: 'TestDB'
+            }
+        }
+    }
+
+}
