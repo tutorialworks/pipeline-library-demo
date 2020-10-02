5a962d7bc470c2a71e1774c67efae06d7d874062
Ramagopalan <Ramji_Ramagopalan@Dell.com>
2020-10-02 09:01:34 +0530

    post to influxdb

--- vars/checkout.groovy
+++ vars/checkout.groovy
@@ -5,6 +5,8 @@ stage('Checkout') {
     echo "Source code checkout"
     def stagename='Checkout'
     echo "$stagename is completed"
+    steps{
     influxDbPublisher customPrefix: '', customProjectName: 'TestDB', jenkinsEnvParameterField: 'KEY=\'testdata\'', jenkinsEnvParameterTag: '', selectedTarget: 'TestDB'
+}
 }
 }
