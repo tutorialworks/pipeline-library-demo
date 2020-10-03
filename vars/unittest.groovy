8482596680983c7f09d22c88b9c68e26def2dd7f
Ramagopalan <Ramji_Ramagopalan@Dell.com>
2020-10-04 00:38:36 +0530

    test

--- vars/unittest.groovy
+++ vars/unittest.groovy
@@ -1,10 +1,18 @@
 #!/usr/bin/env groovy
 def call(){
 node('master') {
+	environment{
+		BUILD_ID=${BUILD_ID}
+		BUILD_URL${BUILD_URL}
+			}
+	stage('Display'){
+		echo  "Build id is ${BUILD_ID}"
+		echo "Build Url is ${BUILD_URL}"
+	}
    
    stage('UNIT TEST') {
    	echo "Unit testing the code"
-    influxDbPublisher customPrefix: '', customProjectName: '', jenkinsEnvParameterField: 'KEY=${BUILD_ID}', jenkinsEnvParameterTag: 'KEY=${BUILD_URL}', selectedTarget: 'js1'
+    influxDbPublisher customPrefix: '', customProjectName: '', jenkinsEnvParameterField: 'KEY=${BUILD_ID}', jenkinsEnvParameterTag: 'KEY=${BUILD_URL}', selectedTarget: 'TestDB'
 }
 
 }
