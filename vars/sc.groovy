2cab0f490c5d8ae644eb1e32d2eebe7f57e72a6b
Ramagopalan <Ramji_Ramagopalan@Dell.com>
2020-10-03 14:29:29 +0530

    add

--- vars/sc.groovy
+++ vars/sc.groovy
@@ -5,14 +5,15 @@ node('master') {
         BUILD_URL = "${BUILD_URL}"
 
     }
-    def assignURL(build_url) {
-    def buildURL = [:]
-    buildURL['url'] = build_url
-    return buildURL}
+
    stage('Checkout') {
    	echo "Checkout source code"
    	echo "${BUILD_URL}"
     influxDbPublisher(selectedTarget: 'TestDB', customData: assignURL(BUILD_URL))
+    def assignURL(build_url) {
+    def buildURL = [:]
+    buildURL['url'] = build_url
+    return buildURL}
 
 }
 
