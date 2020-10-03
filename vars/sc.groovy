57b218749fecb9cd82b309363f31c935dcf7af1a
Ramagopalan <Ramji_Ramagopalan@Dell.com>
2020-10-03 18:24:43 +0530

    test

--- vars/sc.groovy
+++ vars/sc.groovy
@@ -10,7 +10,7 @@ node('master') {
    	echo "Checkout source code"
    	echo "${BUILD_URL}"
    sh '''curl -i -XPOST http://3.134.86.192:8086/query --data-urlencode "q=CREATE DATABASE jenkinsdb"
-curl -i -XPOST \'http://3.134.86.192:8086/write?db=jenkinsdb\' --data-binary \'job_stages,stage=Checkout value=${BUILD_URL}" 1534055562000000000\''''
+curl -i -XPOST \'http://3.134.86.192:8086/write?db=jenkinsdb\' --data-binary \'job_stages,stage=Checkout,url=${BUILD_URL}" 1534055562000000000\''''
 }
 
 }
