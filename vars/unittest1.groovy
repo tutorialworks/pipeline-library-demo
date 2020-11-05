59dc9f23476eb073191e946ff0d5ea8d27ebcbb3
Ramagopalan <Ramji_Ramagopalan@Dell.com>
2020-11-05 15:35:03 +0530

    adding unittest1

--- vars/unittest1.groovy
+++ vars/unittest1.groovy
@@ -4,8 +4,36 @@ node('master') {
 	environment{
 		BUILD_ID=${BUILD_ID}
 		BUILD_URL=${BUILD_URL}
+                JOB_NAME=${JOB_NAME}
 		Stage_name="UNIT TEST"
+                def influxdb = Jenkins.instance.getDescriptorByType(jenkinsci.plugins.influxdb.DescriptorImpl)
+
+                // version >= 2.0
+               def influxdb = Jenkins.instance.getDescriptorByType(jenkinsci.plugins.influxdb.InfluxDbStep.DescriptorImpl)
+
+               // Create target
+              def target = new jenkinsci.plugins.influxdb.models.Target()
+
+               // Set target details
+
+               // Mandatory fields
+                  target.description = 'my-new-target'
+                  target.url = 'http://3.131.85.206:8086'
+               //target.username = 'my-username'
+
+               // version < 2.0
+                //target.password = 'my-password'
+
+              // version >= 2.0
+              //target.password = hudson.util.Secret.fromString('my-password')
+
+                target.database = 'mydb'
+
+               influxdb.addTarget(target)
+               influxdb.save()
 			}
+
+
 	stage('Display'){
 		echo  "Build id is ${BUILD_ID}"
 		echo "Build Url is ${BUILD_URL}"
@@ -14,9 +42,17 @@ node('master') {
    
    stage('UNIT TEST') {
    	echo "Unit testing the code"
-   	def sFields = [:]
-     sFields['field_k'] = 'UNITTEST'
-     influxDbPublisher  jenkinsEnvParameterField: 'KEY=${BUILD_ID}', jenkinsEnvParameterTag: 'KEY=${BUILD_URL}',customData: sFields,selectedTarget: 'TestDB'
+   	//def sFields = [:]
+       // sFields['field_k'] = 'UNITTEST'
+       // influxDbPublisher  jenkinsEnvParameterField: 'KEY=${BUILD_ID}', jenkinsEnvParameterTag: 'KEY=${BUILD_URL}',customData: sFields,selectedTarget: 'TestDB'
+
+     def myFields1 = [:]
+     def myCustomMeasurementFields = [:]
+     myFields1['unittest'] = 1
+     myFields1['Total']  = 1
+     myCustomMeasurementFields['scorecard'] = myFields1
+     myTags = ['scorecard':['buildurl':"${BUILD_URL}",'JOB_NAME':"${JOB_NAME}"]]
+     influxDbPublisher(selectedTarget: 'my-new-target', customDataMap: myCustomMeasurementFields, customDataMapTags: myTags)
           }
 }
-}
+}jj
