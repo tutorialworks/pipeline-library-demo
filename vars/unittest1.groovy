d20442ab8c4f7e5e3b7d10ea61518b11e1c242da
Ramagopalan <Ramji_Ramagopalan@Dell.com>
2020-11-05 15:41:20 +0530

    adding unittest1

--- vars/unittest1.groovy
+++ vars/unittest1.groovy
@@ -9,6 +9,46 @@ node('master') {
                 //def influxdb = Jenkins.instance.getDescriptorByType(jenkinsci.plugins.influxdb.DescriptorImpl)
 
                 // version >= 2.0
+               //def influxdb = Jenkins.instance.getDescriptorByType(jenkinsci.plugins.influxdb.InfluxDbStep.DescriptorImpl)
+
+               // Create target
+              //def target = new jenkinsci.plugins.influxdb.models.Target()
+
+               // Set target details
+
+               // Mandatory fields
+               //   target.description = 'my-new-target'
+              //    target.url = 'http://3.131.85.206:8086'
+               //target.username = 'my-username'
+
+               // version < 2.0
+                //target.password = 'my-password'
+
+              // version >= 2.0
+              //target.password = hudson.util.Secret.fromString('my-password')
+
+                //target.database = 'mydb'
+
+               //influxdb.addTarget(target)
+               //influxdb.save()
+			}
+
+
+	stage('Display'){
+		echo  "Build id is ${BUILD_ID}"
+		echo "Build Url is ${BUILD_URL}"
+		echo "Stage_name is ${Stage_name}"
+	}
+   
+   stage('UNIT TEST') {
+   	echo "Unit testing the code"
+    BUILD_ID=${BUILD_ID}
+    BUILD_URL=${BUILD_URL}
+                JOB_NAME=${JOB_NAME}
+    Stage_name="UNIT TEST"
+                //def influxdb = Jenkins.instance.getDescriptorByType(jenkinsci.plugins.influxdb.DescriptorImpl)
+
+                // version >= 2.0
                def influxdb = Jenkins.instance.getDescriptorByType(jenkinsci.plugins.influxdb.InfluxDbStep.DescriptorImpl)
 
                // Create target
@@ -31,17 +71,6 @@ node('master') {
 
                influxdb.addTarget(target)
                influxdb.save()
-			}
-
-
-	stage('Display'){
-		echo  "Build id is ${BUILD_ID}"
-		echo "Build Url is ${BUILD_URL}"
-		echo "Stage_name is ${Stage_name}"
-	}
-   
-   stage('UNIT TEST') {
-   	echo "Unit testing the code"
    	//def sFields = [:]
        // sFields['field_k'] = 'UNITTEST'
        // influxDbPublisher  jenkinsEnvParameterField: 'KEY=${BUILD_ID}', jenkinsEnvParameterTag: 'KEY=${BUILD_URL}',customData: sFields,selectedTarget: 'TestDB'
